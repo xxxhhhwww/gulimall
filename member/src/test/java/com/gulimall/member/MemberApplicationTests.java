773b5ecee1dd192d5fe8dfea1c67be7f49d04e33
tom <tom@qq.com>
2022-08-19 09:54:50 +0800

    项目初始结构创建

---
+++ member/src/test/java/com/gulimall/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
